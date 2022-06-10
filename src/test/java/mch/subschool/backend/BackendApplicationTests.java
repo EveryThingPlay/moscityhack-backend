254da9da2ffae7e365f6c23693309cc4ededc972
astrelets <astrelets@t1-consulting.ru>
2022-06-10 21:49:18 +0300

    Первоначальная структура

---
+++ src/test/java/mch/subschool/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mch.subschool.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
