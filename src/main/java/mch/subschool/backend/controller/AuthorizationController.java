604388d21e0ebdea3e5bfddcc3ecb820aad82f96
astrelets <astrelets@t1-consulting.ru>
2022-06-13 01:43:55 +0300

    Добавил CROSS

--- src/main/java/mch/subschool/backend/controller/AuthorizationController.java
+++ src/main/java/mch/subschool/backend/controller/AuthorizationController.java
@@ -8,12 +8,10 @@ import mch.subschool.backend.mapper.ProfileMapper;
 import mch.subschool.backend.service.LogInService;
 import mch.subschool.backend.service.ProfileService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/authorization/")
 @RequiredArgsConstructor
 public class AuthorizationController {
