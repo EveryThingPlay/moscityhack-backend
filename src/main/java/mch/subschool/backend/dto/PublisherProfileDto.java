f609e2d3c4652c6ab2647eb303e8a10d5cb2232d
astrelets <astrelets@t1-consulting.ru>
2022-06-12 21:45:50 +0300

    Рефакторинг

--- src/main/java/mch/subschool/backend/dto/PublisherProfileDto.java
+++ src/main/java/mch/subschool/backend/dto/PublisherProfileDto.java
@@ -2,8 +2,6 @@ package mch.subschool.backend.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import mch.subschool.backend.common.profile.ProfileType;
-import mch.subschool.backend.model.profile.publisher.PublisherProfileInfo;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
--- src/main/java/mch/subschool/backend/service/TokenService.java
+++ src/main/java/mch/subschool/backend/service/TokenService.java
@@ -2,9 +2,6 @@ package mch.subschool.backend.service;
 
 import mch.subschool.backend.common.ProtectedWebResource;
 
-import java.util.function.Function;
-
 public interface TokenService {
     boolean isTokenAdmittedForResource(String token, ProtectedWebResource resource);
-    <T,R> R getResultIfTokenValid(String token, ProtectedWebResource resource, Function<T, R> function, T input) throws IllegalAccessException;
 }
